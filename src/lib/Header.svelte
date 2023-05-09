b27161c3bf957dd8285a90381fa2871068cbb353
@s.roertgen <sroertgen@gmail.com>
2023-05-09 16:14:23 +0200

    add login button

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,15 @@
 <script>
-		import SearchHeader from "./Search/SearchHeader.svelte";
-
+	import SearchHeader from './Search/SearchHeader.svelte';
+	import { login } from '$lib/nostr/login.js';
 </script>
 
-<div class="sticky top-0 z-50 navbar bg-base-100">
-	<a href="/" class=" absolute top-0 btn btn-ghost normal-case text-xl">Bitcoin-Library</a>
-  <div class="mx-auto">
+<div class="navbar sticky top-0 z-50 bg-base-100">
+	<a href="/" class=" btn-ghost btn absolute top-0 text-xl normal-case"
+		>Bitcoin-Library</a
+	>
+	<div class="mx-auto">
 		<SearchHeader />
 	</div>
+	<button class="btn ml-auto mr-5" on:click={login}>Login</button>
 </div>
+
