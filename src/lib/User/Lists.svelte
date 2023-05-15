eed608e3702be493129bf51f07531ff40944cb77
@s.roertgen <sroertgen@gmail.com>
2023-05-15 15:44:12 +0200

    add removal function for lists

--- src/lib/User/Lists.svelte
+++ src/lib/User/Lists.svelte
@@ -2,18 +2,40 @@
 	export let lists;
 
 	import { sortListOfObjects } from '$lib/utils.js';
+	import { removeList } from '$lib/nostr/removeList.js';
 
 	$: console.log(lists);
 </script>
 
 {#each sortListOfObjects(lists) as list}
 	<div class="rounded border border-solid border-white p-2">
-		<p class="text-xl font-bold">
-			#
-			<span class="underline">
-				{list.tags.find((l) => l[0] === 'd')[1]}
-			</span>
-		</p>
+		<div class="flex flex-row">
+			<p class="text-xl font-bold">
+				#
+				<span class="underline">
+					{list.tags.find((l) => l[0] === 'd')[1]}
+				</span>
+			</p>
+			<button
+				on:click={removeList(list.id)}
+				class="btn-sm btn-circle btn ml-auto mr-1 bg-red-500"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					style="color: white"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/></svg
+				>
+			</button>
+		</div>
 		{#each list.tags.filter((l) => l[0] !== 'd') as tag}
 			<div class="m-2 rounded border border-solid border-white p-2">
 				<p>{tag[1]}</p>
