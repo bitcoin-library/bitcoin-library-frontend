ead1de0f9a987b49868b092aa2490c2def40ee38
@s.roertgen <sroertgen@gmail.com>
2023-05-15 15:26:20 +0200

    add comonent for lists and sort lists

---
+++ src/lib/User/Lists.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let lists;
+
+	import { sortListOfObjects } from '$lib/utils.js';
+
+	$: console.log(lists);
+</script>
+
+{#each sortListOfObjects(lists) as list}
+	<div class="rounded border border-solid border-white p-2">
+		<p class="text-xl font-bold">
+			#
+			<span class="underline">
+				{list.tags.find((l) => l[0] === 'd')[1]}
+			</span>
+		</p>
+		{#each list.tags.filter((l) => l[0] !== 'd') as tag}
+			<div class="m-2 rounded border border-solid border-white p-2">
+				<p>{tag[1]}</p>
+			</div>
+		{/each}
+	</div>
+{/each}
