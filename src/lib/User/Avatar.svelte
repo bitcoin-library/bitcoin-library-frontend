49e7de66f19d8e2aadcf75557076e49da7604e86
@s.roertgen <sroertgen@gmail.com>
2023-07-06 14:35:15 +0200

    improve a lot of stuff, especially lists

--- src/lib/User/Avatar.svelte
+++ src/lib/User/Avatar.svelte
@@ -3,24 +3,22 @@
 </script>
 
 <div class="avatar">
-	<div
-		on:click={() => {
-			if ($openDetailbar === true && Object.keys($selectedCard).length) {
-				selectedCard.set({});
-				user.update((u) => ({ ...u, showDetails: true }));
-			} else if (
-				$openDetailbar === true &&
-				!Object.keys($selectedCard).length
-			) {
-				$openDetailbar = false;
-				user.update((u) => ({ ...u, showDetails: false }));
-			} else {
-				$openDetailbar = true;
-				user.update((u) => ({ ...u, showDetails: true }));
-			}
-		}}
-		class="cursor-pointer rounded-full"
-	>
+	<div class="cursor-pointer rounded-full">
+		<!-- on:click={() => { -->
+		<!-- 	if ($openDetailbar === true && Object.keys($selectedCard).length) { -->
+		<!-- 		selectedCard.set({}); -->
+		<!-- 		user.update((u) => ({ ...u, showDetails: true })); -->
+		<!-- 	} else if ( -->
+		<!-- 		$openDetailbar === true && -->
+		<!-- 		!Object.keys($selectedCard).length -->
+		<!-- 	) { -->
+		<!-- 		$openDetailbar = false; -->
+		<!-- 		user.update((u) => ({ ...u, showDetails: false })); -->
+		<!-- 	} else { -->
+		<!-- 		$openDetailbar = true; -->
+		<!-- 		user.update((u) => ({ ...u, showDetails: true })); -->
+		<!-- 	} -->
+		<!-- }} -->
 		<img src={$user?.profile?.image} />
 	</div>
 </div>
