876d5811e5032143309b4028e4f59a90a16240a5
@s.roertgen <sroertgen@gmail.com>
2023-07-21 08:51:59 +0200

    remove some console log statements

--- src/lib/nostr/addResourceToLists.js
+++ src/lib/nostr/addResourceToLists.js
@@ -11,8 +11,6 @@ import { getSelectedListFromID, getListName, getListEvents } from "./lists/utils
  * @param {string} resourceEventId - Id of the event to add to the list
  */
 export const addResourceToLists = async (selectedListIDs, resourceEventId, index = null) => {
-  console.log("adding a resource to the list")
-  console.log("selectedListIDs", selectedListIDs)
   // FIXME reuse ndk object from ndk.js
   const nip07signer = new NDKNip07Signer();
   ndk.signer = nip07signer
@@ -22,16 +20,10 @@ export const addResourceToLists = async (selectedListIDs, resourceEventId, index
     }
   });
   const promises = selectedListIDs.map(async (listID) => {
-    console.log(get(user))
-    console.log("listId", listID)
     const event = new NDKEvent(ndk);
     const selectedList = getSelectedListFromID(get(user).lists, listID)
-    console.log("selected List", selectedList)
     const listName = getListName(selectedList)
-    console.log("listName", listName)
     const existingEvents = getListEvents(selectedList)
-    console.log("existingEvents", existingEvents)
-    console.log(resourceEventId)
     event.kind = 30001;
     event.content = "";
     if (index === null) {
@@ -43,18 +35,14 @@ export const addResourceToLists = async (selectedListIDs, resourceEventId, index
       ]
     } else {
       existingEvents.splice(index + 1, 0, ["e", resourceEventId]);
-      console.log("inserted event in existing events", existingEvents)
       event.tags = [
         ["d", listID],
         ["name", listName],
         ...existingEvents
       ]
-      console.log("tags", event.tags)
     }
-    console.log(event)
     await event.sign(ndk.signer)
     await ndk.publish(event)
-    console.log("published list")
     // update lists
   });
   await Promise.all(promises)
